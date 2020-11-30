cbe923dff6df508e2e6ca4e2415b7a6afda06000
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2020-11-30 01:21:21 +0100

    settings inline feedback

--- src/Tools/Settings/Settings.js
+++ src/Tools/Settings/Settings.js
@@ -1,21 +1,44 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { setShowWelcome } from "../../actions";
 import { RootContext } from "../../context";
+import InlineFeedback from "../../InlineFeedback";
 import "./Settings.css"
 
 function Settings() {
   const { dispatch, store } = useContext(RootContext);
   const { showWelcome } = store;
 
+  const [status, setStatus] = useState(null);
+
   const triggerWelcomeClick = () => {
     dispatch(setShowWelcome(!showWelcome));
+  }
+
+  const onSendResponse = res => {
+    let msg =
+    res.code === 200
+      ? "Thank you!"
+      : "Couldn't send feedback";
+    setStatus({ message: msg, code: res.code });
   }
 
   return (
     <div className="settings panel">
      <ul>
-       <li>
-         <p>This section is a work-in-progress. What settings would you find useful being customizable? Send your feedback clicking the loudspeaker icon on the sidebar.</p>
+       <li className="feedback-item">
+         <p>This section is a work-in-progress.
+           What settings would you find useful being customizable?
+          <small className={!!status && status.code === 200 ? "success" : ""}>
+            {!!status && !!status.message ? status.message : ""}
+          </small>
+          </p>
+         <InlineFeedback
+          description=""
+          placeholder="I'd to be able to..."
+          type="settings_ideas"
+          className="settings-feedback"
+          onSendResponse={onSendResponse}
+         />
       </li>
      </ul>
       <ul>
